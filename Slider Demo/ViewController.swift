9f40f1a03c52f1f6301c0f0bc2ea6eaf7305a2f2
trungld3 <k21tpm4@gmail.com>
2020-05-18 16:06:26 +0700

    indexPatch.row

--- Slider Demo/ViewController.swift
+++ Slider Demo/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var table: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = String ((Int(slider.value * 20)) * (indexPath.row + 1))
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func silderChanged(_ sender: Any) {
+        table.reloadData()
+    }
+    
 }
 
